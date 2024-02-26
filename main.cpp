8465b4f8e3b11a86ed12bb85ef3c143d507db5cf
Yen-Hsiang Chang <yenhsiangc@gmail.com>
2024-02-25 21:22:04 -0800

    reformulate main

--- main.cpp
+++ main.cpp
@@ -18,46 +18,57 @@ int main(int argc, char* argv[]) {
   parlay::random_generator gen(seed);
 
   // macros
-  // using W = double;
-  using W = unsigned int;
+  using W = double;
+  // using W = unsigned int;
 
-  // read the graph
+  // read the graph, assume {u, v} only has (u, v) or (v, u)
   auto G = gbbs::gbbs_io::read_weighted_symmetric_graph<W>(argv[1], false, false);
-    // "/global/cfs/cdirs/mp156/yenhsiang/rhg.txt", false, false);
-    // "/global/cfs/cdirs/mp156/yenhsiang/test.txt", false, false);
-    // "/global/cfs/cdirs/mp156/yenhsiang/largerhg.txt", false, false);
-    // "/global/cfs/cdirs/mp156/yenhsiang/superlargerhg.txt", false, false);
-    // "/global/cfs/cdirs/mp156/yenhsiang/livejournal.txt", false, false);
-  // auto edges = G.edges();
-  // auto edges_with_exp = parlay::tabulate(G.m, [&](const size_t& i) {
-  //   return std::make_pair(edges[i], double(0));
-  // });
+  auto E_MST_input = gbbs::new_array_no_init<std::tuple<gbbs::uintE, gbbs::uintE>>(G.m);
+  parlay::parallel_for(0, G.m, [&](const size_t& i) {
+    E_MST_input[i] = std::make_tuple(std::get<0>(G.e0[i]), 0);
+  });
 
-  // for(int iter = 0;iter < 1;++iter) {
-  //   // Step 1: Generate weighted random edge ordering
-  //   auto weighted_ordering_begin = omp_get_wtime();
-  //   gen();
-  //   parlay::parallel_for(0, G.m, [&](const size_t& i) {
-  //     std::exponential_distribution<double> exponential(std::get<2>(edges_with_exp[i].first));
-  //     auto local_rand = gen[i];
-  //     edges_with_exp[i].second = exponential(local_rand);
-  //   });
+  auto G_MST_input = gbbs::symmetric_graph<gbbs::symmetric_vertex, gbbs::uintE>(
+      G.v_data, G.n, G.m,
+      [](){},
+      E_MST_input);
+  
+  auto E_exponential = parlay::sequence<std::pair<double, gbbs::uintE>>(G.m);
 
-  //   auto pred = [&](const std::pair<std::tuple<gbbs::uintE, gbbs::uintE, W>, double>& l,
-  //                   const std::pair<std::tuple<gbbs::uintE, gbbs::uintE, W>, double>& r) {
-  //                     return l.second < r.second;
-  //                   };
-  //   compSort(edges_with_exp, pred);
-  //   auto weighted_ordering_end = omp_get_wtime();
-  //   std::cout << "Weighted Random Edge Ordering Time: "
-  //             << double(weighted_ordering_end - weighted_ordering_begin) << " seconds" << std::endl;
+  for(int iter = 0;iter < 1;++iter) {
+    // Step 1: Generate weighted random edge ordering
+    auto weighted_ordering_begin = omp_get_wtime();
+    gen();
+    parlay::parallel_for(0, G.m, [&](const size_t& i) {
+      std::exponential_distribution<double> exponential(std::get<1>(G.e0[i]));
+      auto local_rand = gen[i];
+      E_exponential[i] = std::make_pair(exponential(local_rand), gbbs::uintE(i));
+    });
+
+    compSort(E_exponential, std::less<std::pair<double, gbbs::uintE>>());
+
+    parlay::parallel_for(0, G.m, [&](const size_t& i) {
+      std::get<1>(E_MST_input[E_exponential[i].second]) = i; 
+    });
+      
+    auto weighted_ordering_end = omp_get_wtime();
+    std::cout << "Weighted Random Edge Ordering Time: "
+              << double(weighted_ordering_end - weighted_ordering_begin) << " seconds" << std::endl;
   
     // Step 2: Compute MST based on weighted random edge ordering
     auto mst_begin = omp_get_wtime();
-    gbbs::MinimumSpanningForest_boruvka::MinimumSpanningForest(G);
+    auto E_MST = gbbs::MinimumSpanningForest_boruvka::MinimumSpanningForest(G_MST_input);
     auto mst_end = omp_get_wtime();
 
     std::cout << "MST Time: "
               << double(mst_end - mst_begin) << " seconds" << std::endl;
-  // }
+    
+    if (E_MST.size() + 1 != G.n)
+    {
+      std::cout << "min cut = 0" << std::endl; 
+      break;
+    }
+  }
+
+  gbbs::free_array(E_MST_input, G.m);
 }
